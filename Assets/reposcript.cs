6192d7d4f6b740cd8e6811b7644cf561a7d417d2
Nicolla Elkhouri <nicollae@entangleinteractive.com>
2017-03-28 21:21:00 +1100

    reposcript add

---
+++ Assets/reposcript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reposcript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//test repo you
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
